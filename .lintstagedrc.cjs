5b8e7c44af9663a48fab5661d6f8a5776ecfd239
Jace Babin <jbabin91@gmail.com>
2024-03-18 09:18:14 -0700

    feat(frontend): :sparkles: add frontend project

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -2,8 +2,8 @@ module.exports = {
   // Type check TypeScript files
   '(apps|packages|tooling)/**/*.(ts|tsx)': () => 'pnpm typecheck',
   // Lint then format TypeScript and JavaScript files
-  '(apps|packages|tooling)/**/*.(ts|tsx|js|jsx|json)': (files) => [
-    `pnpm eslint --max-warnings 0 ${files.join(',')}`,
-    `pnpm prettier -uc ${files.join(',')}`,
+  '(apps|packages|tooling)/**/*.(ts|tsx|js)': (files) => [
+    `pnpm eslint --max-warnings 0 ${files.join(' ')}`,
+    `pnpm prettier -uc ${files.join(' ')}`,
   ],
 };
