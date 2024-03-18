e7c38d1a3bb6af8831c96155ea617f14a83c3ab7
Jace Babin <jbabin91@gmail.com>
2024-03-17 22:52:44 -0700

    chore(repo) update lint-staged rules
    Signed-off-by: Jace Babin <jbabin91@gmail.com>

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -2,8 +2,8 @@ module.exports = {
   // Type check TypeScript files
   '(apps|packages|tooling)/**/*.(ts|tsx)': () => 'pnpm typecheck',
   // Lint then format TypeScript and JavaScript files
-  '(apps|packages|tooling)/**/*.(ts|tsx|js)': (filenames) => [
-    `pnpm prettier -uc ${filenames.join(' ')}`,
-    `pnpm eslint ${filenames.join(' ')}`,
+  '(apps|packages|tooling)/**/*.(ts|tsx|js|jsx|json)': (files) => [
+    `pnpm eslint --max-warnings 0 ${files.join(',')}`,
+    `pnpm prettier -uc ${files.join(',')}`,
   ],
 };
